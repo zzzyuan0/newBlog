5083fcfba902b0b67c9d4577c424eb08d545535e
luoxq <supercodeman@foxmail.com>
2022-07-17 22:40:33 +0800

    新增token 认证

---
+++ newblogApi/newblog-user/src/main/java/cn/zzzyuan/user/service/TokenService.java
@@ -0,0 +1,17 @@
+package cn.zzzyuan.user.service;
+
+import cn.zzzyuan.common.entity.Token;
+
+/**
+ * @author codesuperman@foxmail.com
+ * @date 2021-11-14
+ */
+public interface TokenService {
+
+    /**
+     * 获取token
+     * @return
+     */
+    public Token getToken(String username, String password);
+
+}
