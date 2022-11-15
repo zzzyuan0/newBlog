eb3a54b56d53497efaa617f189ce7417b52b6a48
luoxq <1537069101@qq.com>
2022-11-15 20:24:34 +0800

    修改配置以及依赖，redis使用jedis，解决连接超时

--- newblogApi/newblog-blog/src/main/java/cn/zzzyuan/exception/ExceptionHandler.java
+++ newblogApi/newblog-blog/src/main/java/cn/zzzyuan/exception/ExceptionHandler.java
@@ -3,7 +3,6 @@ package cn.zzzyuan.exception;
 import cn.zzzyuan.common.content.ResponseContent;
 import cn.zzzyuan.common.entity.ResponseResult;
 import cn.zzzyuan.common.exception.GlobalExceptionHandler;
-import io.lettuce.core.RedisCommandTimeoutException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,11 +17,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class ExceptionHandler extends GlobalExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(RedisCommandTimeoutException.class)
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseResult missingServletRequestParameterException(RedisCommandTimeoutException e){
-        log.error("{}--{}，异常类型--{}",ResponseContent.ERROR.msg(), e.getMessage(),e.getClass());
-        return ResponseResult.error(ResponseContent.ERROR.msg(), ResponseContent.ERROR.code(),null);
-    }
+
 
 }
